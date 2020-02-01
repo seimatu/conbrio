7688f8e432f098f744d9590169b0a630c8800ba4
松本世 <matumoto.p04a@gmail.com>
2020-02-01 16:59:36 +0900

    ten

--- app/views.py
+++ app/views.py
@@ -20,7 +20,7 @@ def city(request,city_name):
     return render(request,'app/city.html',{'planic':planic,'city_name':city_name})
 
 def plan_list(request,plan_categories):
-    plan_categories=Category.objects.get(title=plan_categories)[0]
+    plan_categories=Category.objects.get(title=plan_categories)
     planning=Plan.objects.filter(plan_categories=plan_categories).order_by('-created_at')
     return render(request,'app/plan_list.html',{'planning':planning,'plan_categories':plan_categories})
 
