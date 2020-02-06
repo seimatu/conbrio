cc62efa966ccc9b6e929a42f6f60f4fd125a9771
松本世 <matumoto.p04a@gmail.com>
2020-02-06 16:41:33 +0900

    17

--- conbrio6/settings.py
+++ conbrio6/settings.py
@@ -137,11 +137,10 @@ USE_TZ = True
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'staticfiles')]
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
